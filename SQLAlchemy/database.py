29e7a92abde3bea346522941afe0d6585e0a8f58
Zahra Nasr <zahra.nasr97@gmail.com>
2023-10-26 15:59:33 +0330

    add PostgreSQL instead of sqlite

--- SQLAlchemy/database.py
+++ SQLAlchemy/database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
+from config import settings
 from sqlalchemy.orm import declarative_base, Session
 
 Base = declarative_base()
-engine = create_engine("sqlite:///metro.sqlite3")
+engine = create_engine(f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}")
 
 def db_add_obj(obj):
     with Session(engine) as session:
