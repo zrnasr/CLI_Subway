feffde1343496595106bb8fbd109a176abc32545
Zahra Nasr <zahra.nasr97@gmail.com>
2023-10-26 20:30:22 +0330

    dont create database if existed.

--- SQLAlchemy/database.py
+++ SQLAlchemy/database.py
@@ -1,9 +1,14 @@
 from sqlalchemy import create_engine
 from config import settings
+from sqlalchemy_utils.functions import database_exists, create_database
 from sqlalchemy.orm import declarative_base, Session
 
+engine_url = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/subway"
+
 Base = declarative_base()
-engine = create_engine(f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}")
+engine = create_engine(engine_url)
+if not database_exists(engine.url):
+    create_database(engine.url)
 
 def db_add_obj(obj):
     with Session(engine) as session:
